00914be4734e6a8d4c035705ee297eea24596150
mateotis <hekfuszmate@gmail.com>
2022-02-15 21:33:42 +0400

    30MFF page code finished, hopefully!

--- movie-premier.js
+++ movie-premier.js
@@ -1,15 +1,12 @@
-console.log('hello!');
-
+// Click-through code to gradually reveal the page through use of the 'visibility' attribute
 let button1 = document.getElementById("button1");
-
 button1.addEventListener('click', () => {
 
-	let line = document.getElementsByClassName("promo promo2")[0];
+	let line = document.getElementsByClassName("promo promo2")[0]; // Since getElementsByClassName() returns a list, we have to specify which element we pick (there should only be one in this case)
 	line.style.visibility = 'visible';
 });
 
 let button2 = document.getElementById("button2");
-
 button2.addEventListener('click', () => {
 
 	let line = document.getElementsByClassName("promo promo3")[0];
@@ -17,11 +14,8 @@ button2.addEventListener('click', () => {
 });
 
 let button3 = document.getElementById("button3");
-
 button3.addEventListener('click', () => {
 
-	let line = document.getElementsByClassName("vid-title")[0];
-	line.style.visibility = 'visible';
-
-	document.getElementsByClassName("content")[0].style.visibility = 'visible';
+	let content = document.getElementsByClassName("content")[0];
+	content.style.visibility = 'visible';
 });

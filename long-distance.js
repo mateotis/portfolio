12e8e62e689ac525c930743aab3d74aa34b4351f
mateotis <hekfuszmate@gmail.com>
2022-03-04 20:52:25 +0400

    Created a story told through scrolling.

---
+++ long-distance.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+messenger = document.getElementsByClassName("messenger")[0];
+
+document.addEventListener("aos:in:color-change-setup", ({detail}) => {
+	messenger.style.backgroundColor = "lightgrey";
+});
+
+document.addEventListener("aos:in:color-change-box1", ({detail}) => {
+	messenger.style.backgroundColor = "darkgrey";
+});
+
+document.addEventListener("aos:in:color-change-box2", ({detail}) => {
+	messenger.style.backgroundColor = "#333";
+});
+
+document.addEventListener("aos:in:color-change-box3", ({detail}) => {
+	messenger.style.backgroundColor = "white";
+});
+
+//for(let i = 0; i < 10; i++) {
+//	document.getElementById("title-dashes").innerHTML += "-";
+//}
